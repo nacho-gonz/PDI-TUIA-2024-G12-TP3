8bb9fa5cd09cc642456bdb9c83245eda79eae769
Ignacio <nacho.gonzalez02410@gmail.com>
2024-12-04 17:19:46 -0300

    cambios codigo final

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# PDI-TUIA-2024-G12-TP3
+# Instrucciones.
+
+## Instalacion 
+
+- *1* **clonar el repositorio**
+
+git clone https://github.com/nacho-gonz/PDI-TUIA-2024-G12-TP3.git
+
+- *2* **Instalar las librerias necesarias**:
+  - opencv: mediante pip: *pip install opencv-contrib-Python*
+  - numpy: mediante pip: *pip install numpy*
+  - matplotlib: mediante pip: *pip install matplotlib*
+
+## Ejecución del código:
+
+  Escribir en la terminal: python TP3-PDI-2024-G13.py
+
+## Personalización
+
+- Si desea agregar videos de tiradas, para detectar los valores de los dados, es necesario que sean añadidas en la carpeta "videos" siguiendo el orden númerico ya existente desde 1 hasta inf y el formato de "tirada_{n}.mp4" siendo n el número del video después de los 4 por default.
+- Ejemplo: si añade su primer video nuevo tendría que llamarse "tirada_5.mp4".
+- Todos los videos de salida van a quedar en la carpeta videos_salida y en caso de repetir el proceso estando los videos dentro, se reescribiran.
--- TP3-PDI-2024-G13.py
+++ TP3-PDI-2024-G13.py
@@ -34,137 +34,139 @@ def filtrar_consecutivos(lista: list)-> list:
 
     return resultado
 
+num_videos = len(os.listdir('./videos'))
 
+if num_videos > 0:
 
-for i in range(1, 5):
-    
-    cap = cv2.VideoCapture(f'videos/tirada_{i}.mp4')
-    # Creamos el objeto de cada video y obtenemos los frames con las funciones que fueron dadas 
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(cap.get(cv2.CAP_PROP_FPS))
-
-    # Creamos el video de salida con los dados remarcados y sus respectivos puntos
-    out = cv2.VideoWriter(f'videos/tirada_output_{i}.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (width,height))
-
-
-    frame_number = 0
-    frames = []
-    centroides_dados = []
-    frames_quietos = []
-    centroides_boundings = []
-
-    while (cap.isOpened()): # Verifica si el video se abrió correctamente.
-
-        ret, frame = cap.read() # 'ret' indica si la lectura fue exitosa (True/False) y 'frame' contiene el contenido del frame si la lectura fue exitosa.
-        if ret == True:  
-            
-            # Agregamos todos los frames del video a una lista
-            frames.append(frame)
-
-            # Decidimos trabajar con el componente A de CIELab, ya que este canal trabaja bien con los colores rojos y verdes
-            # Pudiendo así, identificar bien el fondo de los dados
-            frame_cielab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
-            _, a , _ = cv2.split(frame_cielab)
+    for i in range(1, num_videos+1):
         
-            # Se aplica un umbral para quedarnos únicamente con el área de las monedas
-            _ , mascara = cv2.threshold(a, 120, 255, cv2.THRESH_BINARY)
+        cap = cv2.VideoCapture(f'videos/tirada_{i}.mp4')
+        # Creamos el objeto de cada video y obtenemos los frames con las funciones que fueron dadas 
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+        # Creamos el video de salida con los dados remarcados y sus respectivos puntos
+        out = cv2.VideoWriter(f'videos_salida/tirada_output_{i}.mp4', cv2.VideoWriter_fourcc(*'mp4v'), fps, (width,height))
+
+
+        frame_number = 0
+        frames = []
+        centroides_dados = []
+        frames_quietos = []
+        centroides_boundings = []
+
+        while (cap.isOpened()): # Verifica si el video se abrió correctamente.
+
+            ret, frame = cap.read() # 'ret' indica si la lectura fue exitosa (True/False) y 'frame' contiene el contenido del frame si la lectura fue exitosa.
+            if ret == True:  
+                
+                # Agregamos todos los frames del video a una lista
+                frames.append(frame)
+
+                # Decidimos trabajar con el componente A de CIELab, ya que este canal trabaja bien con los colores rojos y verdes
+                # Pudiendo así, identificar bien el fondo de los dados
+                frame_cielab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+                _, a , _ = cv2.split(frame_cielab)
             
-            # Buscamos las componentes conectadas para identificar los dados
-            num_labels,_,stats,centroide = cv2.connectedComponentsWithStats(mascara,connectivity=4)
+                # Se aplica un umbral para quedarnos únicamente con el área de las monedas
+                _ , mascara = cv2.threshold(a, 120, 255, cv2.THRESH_BINARY)
+                
+                # Buscamos las componentes conectadas para identificar los dados
+                num_labels,_,stats,centroide = cv2.connectedComponentsWithStats(mascara,connectivity=4)
 
-            dados = []
+                dados = []
 
-            # Iteramos sobre todos los objetos que fueron encontrados como posibles dados
-            # Identificándolos, primero por sus relaciones de aspecto, y también por el área que ocupa cada dado
-            # Luego, buscamos los centroides de los dados, para así, luego, poder identificar en qué momento los dados se dejan de mover
-            for i in range(1,num_labels):
-                x, y, w, h, area = stats[i]
-                if 0.4 < w/h < 1.3 and 3000 < w*h < 12000:
-                    centroidee = centroide[i]
-                    centroidee = centroidee.astype(int)
-                    dados.append(centroidee)
-
-
-            centroides_dados.append(dados)
-
-            # Mediante la distancia de Euclídea, buscamos saber cuándo el dado deja de moverse, comprando el frame actual con el anterior y sus respectivos centroides 
-            # También, nos aseguramos de tener únicamente 5 centroides, o sea, 5 dados para comparar
-            # Al final, agregamos el número del frame y la posición del centroide
-            if len(centroides_dados) > 1:
-                frame_actual = centroides_dados[frame_number]
-                frame_anterior = centroides_dados[frame_number-1]
-                dado_no_movil = 0
-                if len(frame_actual) == 5  and len(frame_anterior) == 5:
-                    for i in range(5):
-                        diff = np.linalg.norm(frame_actual[i] - frame_anterior[i])
-                        if diff <= 5:
-                            dado_no_movil += 1
-                    if dado_no_movil == 5:
-                        frames_quietos.append(frame_number)
-                        centroides_boundings.append((frame_number, centroides_dados[frame_number]))
-            frame_number += 1
-        else:  
-            break  
-
-    cap.release()
+                # Iteramos sobre todos los objetos que fueron encontrados como posibles dados
+                # Identificándolos, primero por sus relaciones de aspecto, y también por el área que ocupa cada dado
+                # Luego, buscamos los centroides de los dados, para así, luego, poder identificar en qué momento los dados se dejan de mover
+                for i in range(1,num_labels):
+                    x, y, w, h, area = stats[i]
+                    if 0.4 < w/h < 1.3 and 3000 < w*h < 12000:
+                        centroidee = centroide[i]
+                        centroidee = centroidee.astype(int)
+                        dados.append(centroidee)
 
 
-    # Buscamos los frames consecutivos que tengan los dados estáticos
-    frames_finales = filtrar_consecutivos(frames_quietos)
-    num_dados = []
+                centroides_dados.append(dados)
 
-    # Iteramos sobre todos los frames, buscando los frames consecutivos que anteriormente guardamos 
-    # Luego, trabajamos sobre el primer frame que tenga los dados estáticos, iterando en cada centroide, o sea, en cada dado
-    for indx, imagen in enumerate(frames):
-        if indx in frames_finales:
-            if frames_finales.index(indx) == 0:
-                imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-                kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
-                imagen_tophat = cv2.morphologyEx(imagen_gris, cv2.MORPH_TOPHAT, kernel)
-                for indice_centroide, centroides in centroides_boundings:
-                    if indx == indice_centroide:
-                        for centroide in centroides:
-                            valor_dado = 0
-                            x,y = centroide
-                            
-                            # Pasamos la imagen a gris y creamos una máscara para utilizar con el método tophat 
-                            # Que resaltará las áreas de la imagen que son más brillantes
+                # Mediante la distancia de Euclídea, buscamos saber cuándo el dado deja de moverse, comprando el frame actual con el anterior y sus respectivos centroides 
+                # También, nos aseguramos de tener únicamente 5 centroides, o sea, 5 dados para comparar
+                # Al final, agregamos el número del frame y la posición del centroide
+                if len(centroides_dados) > 1:
+                    frame_actual = centroides_dados[frame_number]
+                    frame_anterior = centroides_dados[frame_number-1]
+                    dado_no_movil = 0
+                    if len(frame_actual) == 5  and len(frame_anterior) == 5:
+                        for i in range(5):
+                            diff = np.linalg.norm(frame_actual[i] - frame_anterior[i])
+                            if diff <= 5:
+                                dado_no_movil += 1
+                        if dado_no_movil == 5:
+                            frames_quietos.append(frame_number)
+                            centroides_boundings.append((frame_number, centroides_dados[frame_number]))
+                frame_number += 1
+            else:  
+                break  
 
-                            # Recortamos la imagen sumando 50 píxeles a cada lado del centroide 
-                            # Luego realizamos un umbral para diferenciar los puntos del dado, y buscamos sus contornos para luego contarlos 
-                            padding = 50
-                            recorte = imagen_tophat[y-padding:y+padding,x-padding:x+padding]
-                            _, recorte_bin = cv2.threshold(recorte, 100, 255, cv2.THRESH_BINARY)
-                            cont, her = cv2.findContours(recorte_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        cap.release()
 
-                            # Buscamos diferenciar las formas de los puntos, buscando sus factores de forma
-                            for contorno in cont:
-                                area_objetos = cv2.contourArea(contorno)
-                                perimetro_cuadrado_objetos = cv2.arcLength(contorno, True)**2
-                                if perimetro_cuadrado_objetos > 0:
-                                    factor_de_forma = area_objetos/perimetro_cuadrado_objetos
-                                    if factor_de_forma > 0.062:
-                                        valor_dado += 1
-                            
-                            # Dibujamos los cuadrados de cada dado y la cantidad de puntos en cada dado
-                            cv2.rectangle(imagen, (x-padding,y-padding), (x+padding,y+padding), (255,0,0), 5)
-                            cv2.putText(imagen, str(valor_dado), (x-47,y-53), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5, cv2.LINE_AA)
-                            num_dados.append(valor_dado)
+
+        # Buscamos los frames consecutivos que tengan los dados estáticos
+        frames_finales = filtrar_consecutivos(frames_quietos)
+        num_dados = []
+
+        # Iteramos sobre todos los frames, buscando los frames consecutivos que anteriormente guardamos 
+        # Luego, trabajamos sobre el primer frame que tenga los dados estáticos, iterando en cada centroide, o sea, en cada dado
+        for indx, imagen in enumerate(frames):
+            if indx in frames_finales:
+                if frames_finales.index(indx) == 0:
+                    imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+                    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20,20))
+                    imagen_tophat = cv2.morphologyEx(imagen_gris, cv2.MORPH_TOPHAT, kernel)
+                    for indice_centroide, centroides in centroides_boundings:
+                        if indx == indice_centroide:
+                            for centroide in centroides:
+                                valor_dado = 0
+                                x,y = centroide
+                                
+                                # Pasamos la imagen a gris y creamos una máscara para utilizar con el método tophat 
+                                # Que resaltará las áreas de la imagen que son más brillantes
+
+                                # Recortamos la imagen sumando 50 píxeles a cada lado del centroide 
+                                # Luego realizamos un umbral para diferenciar los puntos del dado, y buscamos sus contornos para luego contarlos 
+                                padding = 50
+                                recorte = imagen_tophat[y-padding:y+padding,x-padding:x+padding]
+                                _, recorte_bin = cv2.threshold(recorte, 100, 255, cv2.THRESH_BINARY)
+                                cont, her = cv2.findContours(recorte_bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+                                # Buscamos diferenciar las formas de los puntos, buscando sus factores de forma
+                                for contorno in cont:
+                                    area_objetos = cv2.contourArea(contorno)
+                                    perimetro_cuadrado_objetos = cv2.arcLength(contorno, True)**2
+                                    if perimetro_cuadrado_objetos > 0:
+                                        factor_de_forma = area_objetos/perimetro_cuadrado_objetos
+                                        if factor_de_forma > 0.062:
+                                            valor_dado += 1
+                                
+                                # Dibujamos los cuadrados de cada dado y la cantidad de puntos en cada dado
+                                cv2.rectangle(imagen, (x-padding,y-padding), (x+padding,y+padding), (255,0,0), 5)
+                                cv2.putText(imagen, str(valor_dado), (x-47,y-53), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5, cv2.LINE_AA)
+                                num_dados.append(valor_dado)
+                    out.write(imagen)
+                else:
+                    # Paro el resto de frames en que los dados están quitos, graficamos los recuadrados y cantidad de puntos 
+                    cont_dado = 0
+                    for indice_centroide, centroides in centroides_boundings:
+                        if indx == indice_centroide:
+                            for centroide in centroides:
+                                x,y = centroide
+                                cv2.rectangle(imagen, (x-padding,y-padding), (x+padding,y+padding), (255,0,), 5)
+                                cv2.putText(imagen, str(num_dados[cont_dado]), (x-47,y-53), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5, cv2.LINE_AA)
+                                cont_dado += 1
+                    out.write(imagen)
+            else:   
                 out.write(imagen)
-            else:
-                # Paro el resto de frames en que los dados están quitos, graficamos los recuadrados y cantidad de puntos 
-                cont_dado = 0
-                for indice_centroide, centroides in centroides_boundings:
-                    if indx == indice_centroide:
-                        for centroide in centroides:
-                            x,y = centroide
-                            cv2.rectangle(imagen, (x-padding,y-padding), (x+padding,y+padding), (255,0,), 5)
-                            cv2.putText(imagen, str(num_dados[cont_dado]), (x-47,y-53), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 5, cv2.LINE_AA)
-                            cont_dado += 1
-                out.write(imagen)
-        else:   
-            out.write(imagen)
 
 
-    out.release()
+        out.release()
